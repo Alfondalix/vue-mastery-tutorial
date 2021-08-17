3c33f11493ce401200644fef18e204a8065a7c60
alfondalix <alfondalix@gmail.com>
2021-08-17 16:34:07 -0300

    VT-5: event handling lesson 5

--- js/main.js
+++ js/main.js
@@ -7,6 +7,18 @@ var app = new Vue({
     inventory: 11,
     onSale: true,
     details: ['80% cotton', '20& polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 1,
+        variantColor: 'green',
+        variantImage: './assets/img/socks-green.png',
+      },
+      {
+        variantId: 2,
+        variantColor: 'blue',
+        variantImage: './assets/img/socks-blue.png',
+      },
+    ],
     sizes: [{
       sizeId: 1,
       sizeLetter: 'S'
@@ -23,6 +35,18 @@ var app = new Vue({
       sizeId: 4,
       sizeLetter: 'XL'
     },
-    ]
-  }
-})
+    ],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    substractToCart() {
+      this.cart -= 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    },
+  },
+});
