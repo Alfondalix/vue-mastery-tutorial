c339c0460a52c5b300cdc29f8c7fef8288743623
alfondalix <alfondalix@gmail.com>
2021-08-17 16:10:10 -0300

    VT-4: v-for lesson 4

--- js/main.js
+++ js/main.js
@@ -6,5 +6,23 @@ var app = new Vue({
     link: 'https://shop.googlemerchandisestore.com/Google+Redesign/Apparel/Mens/Mens+T+Shirts',
     inventory: 11,
     onSale: true,
+    details: ['80% cotton', '20& polyester', 'Gender-neutral'],
+    sizes: [{
+      sizeId: 1,
+      sizeLetter: 'S'
+    },
+    {
+      sizeId: 2,
+      sizeLetter: 'M'
+    },
+    {
+      sizeId: 3,
+      sizeLetter: 'L'
+    },
+    {
+      sizeId: 4,
+      sizeLetter: 'XL'
+    },
+    ]
   }
 })
