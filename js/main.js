3f61b78fe1979fd05b6916099fe228af4d375caa
alfondalix <alfondalix@gmail.com>
2021-08-17 15:39:59 -0300

    VT-2: lesson 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Socks'
+    product: 'Socks',
+    image: './assets/img/socks-green.png',
+    link: 'https://shop.googlemerchandisestore.com/Google+Redesign/Apparel/Mens/Mens+T+Shirts'
   }
 })
