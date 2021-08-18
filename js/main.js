4935438c6e49bfb8a8f07ab6022cd890e4198355
alfondalix <alfondalix@gmail.com>
2021-08-18 10:05:48 -0300

    VT-8: components lessson 8

--- js/main.js
+++ js/main.js
@@ -1,45 +1,87 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    brand: 'Vue Mastery',
-    product: 'Socks',
-    selectedVariant: 0,
-    link: 'https://shop.googlemerchandisestore.com/Google+Redesign/Apparel/Mens/Mens+T+Shirts',
-    onSale: true,
-    NoStockClass: 'lined',
-    details: ['80% cotton', '20& polyester', 'Gender-neutral'],
-    variants: [
-      {
-        variantId: 1,
-        variantColor: 'green',
-        variantImage: './assets/img/socks-green.png',
-        variantQuantity: 10,
+
+Vue.component('Product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    }
+  },
+  template: `
+  <div class="product">
+  <div class="product-image">
+    <a :href='link' target="_blank">
+      <img :src='image'>
+    </a>
+  </div>
+  <div class="product-info">
+    <h1> {{ title }} </h1>
+    <p v-if='inStock'>In Stock</p>
+    <p v-else :class="[!inStock ? NoStockClass : '']">Out of Stock</p>
+    <p>Shipping: {{ shipping }}</p>
+    <span v-if='onSale'>On Sale!</span>
+    <ul>
+      <li v-for='detail in details'> {{ detail }}</li>
+    </ul>
+    <div v-for='(variant, index) in variants' :key='variant.variantId' class="color-box"
+      :style='{backgroundColor: variant.variantColor}' @mouseover='updateProduct(index)'>
+    </div>
+    <p>Sizes:</p>
+    <ul>
+      <li v-for='size in sizes' :key='size.sizeId'>{{ size.sizeLetter }}</li>
+    </ul>
+    <!-- <span style="display: flex"> -->
+    <button @click='addToCart' :disabled='!inStock' :class='{disabledButton: !inStock}'>Add to
+      Cart</button>
+    <button @click='substractToCart'>Substract from Cart</button>
+    <div class="cart">
+      <p>Cart({{ cart }})</p>
+    </div>
+    <!-- </span> -->
+  </div>
+</div>
+  `,
+  data() {
+    return {
+      brand: 'Vue Mastery',
+      product: 'Socks',
+      selectedVariant: 0,
+      link: 'https://shop.googlemerchandisestore.com/Google+Redesign/Apparel/Mens/Mens+T+Shirts',
+      onSale: true,
+      NoStockClass: 'lined',
+      details: ['80% cotton', '20& polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 1,
+          variantColor: 'green',
+          variantImage: './assets/img/socks-green.png',
+          variantQuantity: 10,
+        },
+        {
+          variantId: 2,
+          variantColor: 'blue',
+          variantImage: './assets/img/socks-blue.png',
+          variantQuantity: 0,
+        },
+      ],
+      sizes: [{
+        sizeId: 1,
+        sizeLetter: 'S'
       },
       {
-        variantId: 2,
-        variantColor: 'blue',
-        variantImage: './assets/img/socks-blue.png',
-        variantQuantity: 0,
+        sizeId: 2,
+        sizeLetter: 'M'
       },
-    ],
-    sizes: [{
-      sizeId: 1,
-      sizeLetter: 'S'
-    },
-    {
-      sizeId: 2,
-      sizeLetter: 'M'
-    },
-    {
-      sizeId: 3,
-      sizeLetter: 'L'
-    },
-    {
-      sizeId: 4,
-      sizeLetter: 'XL'
-    },
-    ],
-    cart: 0,
+      {
+        sizeId: 3,
+        sizeLetter: 'L'
+      },
+      {
+        sizeId: 4,
+        sizeLetter: 'XL'
+      },
+      ],
+      cart: 0,
+    }
   },
   methods: {
     addToCart() {
@@ -66,6 +108,40 @@ var app = new Vue({
       if (onSale === true) {
         return this.brand + ' ' + this.product
       }
+    },
+    shipping() {
+      if (this.premium) {
+        return 'Free'
+      }
+      return 2.99
     }
   },
 });
+
+Vue.component('ProductDetails', {
+  props: {
+    details: {
+      type: Boolean,
+      required: true,
+    }
+  },
+  template: `
+  <div>
+    <p>{{ details }}</p>
+  </div>
+`,
+  computed: {
+    showDetails() {
+      this.details ? 'This is a pair of socks' : '';
+    },
+  }
+},
+);
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    premium: true,
+    details: true,
+  }
+})
